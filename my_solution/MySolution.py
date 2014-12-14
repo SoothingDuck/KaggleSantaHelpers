5da38a6fd5b892453e680c2cfa4273e9a2c1aec9
Yvan Aillet <yvanaillet@yahoo.fr>
2014-12-14 18:51:58 +0100

    ok

--- my_solution/MySolution.py
+++ my_solution/MySolution.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from toypool import ToyPool
 from elfpool import ElfPool
 
+import datetime
 
 # ======================================================================= #
 # === MAIN === #
@@ -9,15 +13,39 @@ if __name__ == '__main__':
 
     start = time.time()
 
+    # Définition de quelques variables
+    working_date = datetime.date(2014, 1, 1)
+
     NUM_ELVES = 900
     NUM_TOYS = 100000
 
     toy_file = os.path.join(os.getcwd(), '..', 'DATA', 'toys_rev2.csv')
     soln_file = os.path.join(os.getcwd(), '..', 'DATA', 'my_solution_num_elves_%d_num_toys_%d.csv' % (NUM_ELVES, NUM_TOYS))
 
-    myelves = create_elves(NUM_ELVES)
-    #solution_firstAvailableElf(toy_file, soln_file, myelves)
-    solution_firstAvailableIfProductiveElf(toy_file, soln_file, myelves)
-    #solution_smallBigQueueElf(toy_file, soln_file, myelves)
+    # Création du pool d'elfes
+    myelfpool = ElfPool(NUM_ELVES)
+
+    # Création du pool de jouets
+    mytoypool = ToyPool()
+    mytoypool.add_file_content(toy_file, NUM_TOYS)
+
+    # Début de l'algorithme
+    while not mytoypool.empty():
+
+        # Sort Toys available for working day
+        mytoypool.fill_till_day(working_date)
+
+        # Apply strategy for all elves in pool 
+        #  Pour chaque elfe:
+        #   1 jouet au hasard parmi ceux disponibles
+        #   Si on peut traiter l'objet dans la journée, le faire et mettre à jour la date de disponibilité de l'elfe
+        #   Sinon planifier l'objet pour le lendemain matin et le traiter et remplir le reste de la journée avec des objets "courts"
+        while not myelfpool.empty_for_date(working_date):
+            elf = myelfpool.next_available_elf()
+            elf.apply_strategy_for(toypool)
+
+        # Go to next working date
+        working_date = working_date + datetime.timedelta(days=1)
+
 
     print 'total runtime = {0}'.format(time.time() - start)
--- my_solution/toypool.py
+++ my_solution/toypool.py
@@ -3,23 +3,28 @@ import unittest
 class ToyPool:
 
     def __init__(self):
-        pass
+        # ordonné par ordre d'arrivée
+        self.__all_toys = []
+
+        # hash : key durée, value tableau ordonné par date de disponibilité
+
+
+    def 
 
 class ToyPoolTest(unittest.TestCase):
 
     def setUp(self):
-        self.toy_pool = ToyPool(10000)
+        toy_file = os.path.join(os.getcwd(), '..', 'DATA', 'toys_rev2.csv')
+        self.toy_empty_pool = ToyPool()
+        self.toy_filled_pool = ToyPool()
+        self.toy_filled_pool.add_file_content(toy_file, 10)
+
 
     def test_len(self):
 
-        self.assertEqual(len(self.pool), 10000)
+        self.assertEqual(len(self.pool), 10)
 
-    def test_get_toy(self):
 
-        toy = self.toy_pool.pop()
-
-    def test_len_for_day(self):
-
-        self.assertEqual(self.pool.len_for_day(datetime.date(2014, 1, 1))
-
+if __name__ == '__main__':
+    unittest.main()
 
