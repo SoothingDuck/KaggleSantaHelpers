3a4c392840ee14da419fb1e2291a921b9d394ff4
Yvan Aillet <yvanaillet@yahoo.fr>
2015-01-02 17:56:38 +0100

    ok

--- my_solution_third/calcul.sh
+++ my_solution_third/calcul.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-for NB_TOYS in 1000000 2000000
+for NB_TOYS in 10000000
 do
-	for NB_ELVES in 50 150 300 500
+	for NB_ELVES in 500 700 900
 	do
-			for LAST_MINUTE in 60 120
+			for LAST_MINUTE in 120
 			do
 				FILENAME="../DATA/my_solution_third_num_elves_${NB_ELVES}_num_toys_${NB_TOYS}_prod_3_5_minutes_${LAST_MINUTE}.csv"
 
--- print_scoring.R
+++ print_scoring.R
@@ -8,6 +8,8 @@ df$threshold <- sapply(df$filename, function(x) {
   as.numeric(paste(strsplit(x, "_")[[1]][11:12], collapse = "."))
     })
 
+ggplot(subset(df, nb_toys == 2000000)) + geom_point(aes(x=nb_elves, y=minutes, size=score)) + facet_wrap(~ nb_toys)
+
 ggplot(df) + geom_point(aes(x=nb_elves, y=minutes, size=score)) + facet_wrap(~ nb_toys)
 
 
